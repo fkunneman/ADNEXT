e3934acbce5ba4326fddabba2ab9df8e3b56bcad
Florian Kunneman <f.kunneman@let.ru.nl>
2013-09-12 17:56:45 +0200

    extra

--- tweetanalysis/score_sentiment_parties.py
+++ tweetanalysis/score_sentiment_parties.py
@@ -10,16 +10,18 @@ parser.add_argument('-i', action='store', required = True, help = 'File with twe
 
 args = parser.parse_args()
 
-party_sentiment = defaultdict(list)
+party_sentiment = defaultdict(lambda : defaultdict(list))
 
 infile = codecs.open(args.i,"r","utf-8")
 for line in infile.readlines():
     tokens = line.strip().split("\t")
+    day = tokens[0]
     party = tokens[1]
     text = tokens[2]
     sentiment = pattern.nl.sentiment(text)
-    party_sentiment[party].append(sentiment)
+    party_sentiment[party][day].append(sentiment)
 
 for party in party_sentiment.keys():
-    sentiments = party_sentiment[party]
-    print party, np.mean(sentiments)
+    for day in sorted(party_sentiment[party].keys()):
+        sentiments = party_sentiment[party][day]
+        print party, day, np.mean(sentiments)
