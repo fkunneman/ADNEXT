75f95c52f4ad0a365036d305788cfd94c36b6e1c
Florian Kunneman <f.kunneman@let.ru.nl>
2013-09-18 13:43:07 +0200

    bbf

--- classification/classifier.py
+++ classification/classifier.py
@@ -233,11 +233,11 @@ class Classifier():
         
         train=codecs.open(classification_dir + "train","w","utf-8")
         for t in self.training:
-            train.write(" ".join([t["features"],t["label"]]) + "\n")
+            train.write(" ".join([t["features"][0],t["label"]]) + "\n")
         train.close()
         test=codecs.open(classification_dir + "test","w","utf-8")
         for t in self.test:
-            test.write(" ".join([t["features"],t["label"]]) + "\n")
+            test.write(" ".join([t["features"][0],t["label"]]) + "\n")
         test.close()
         if prune or select:
             stoplist=codecs.open(classification_dir + "stoplist.txt","w","utf-8")
@@ -254,12 +254,12 @@ class Classifier():
                 label = t["label"]
                 if label == "before":
                     tl=t["meta"][4]
-                    train.write(" ".join([t["features"],tl]) + "\n")
+                    train.write(" ".join([t["features"][0],tl]) + "\n")
             train.close()
             test=codecs.open(classification_dir + "test","w","utf-8")
             for i,t in enumerate(self.test):
                 tl=t["meta"][4]
-                test.write(" ".join([t["features"],tl]) + "\n")
+                test.write(" ".join([t["features"][0],tl]) + "\n")
             test.close()
             stoplist=codecs.open(classification_dir + "stoplist.txt","w","utf-8")
             for feature in self.stoplist:
