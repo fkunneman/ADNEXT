f299295c4675489f0b425b84e269a678f61e7c1e
Florian Kunneman <f.kunneman@let.ru.nl>
2013-09-19 11:04:34 +0200

    ch

--- classification/classifier.py
+++ classification/classifier.py
@@ -476,6 +476,7 @@ class Classifier():
                     if log:
                         if sum(half1) == 1 or sum(half2) == 1:
                             value = 0
+                        print math.log(sum(half2),2),math.log(sum(half1),2)
                         value = math.log(sum(half2),2)/math.log(sum(half1),2)
                     else:
                         value = sum(half2)/sum(half1)
