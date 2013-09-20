ff693bb0dee088af34464a18dc55209c9c74084d
Florian Kunneman <f.kunneman@let.ru.nl>
2013-09-20 15:52:57 +0200

    fix

--- classification/classify.py
+++ classification/classify.py
@@ -7,6 +7,7 @@ import os
 import re
 import multiprocessing
 from collections import defaultdict
+import informed_baseline_time
 
 """
 Script to perform classification with a chosen algorithm and parameter settings. The used data should be in the right format.
@@ -19,7 +20,7 @@ parser.add_argument('-n', action='store', required=False, help="[N-FOLD] specify
 parser.add_argument('-l', action='store', required=False, nargs="+", help="[LOOE] specify the type of leave-one-out (regular,inner_domain or outer_domain), a meta-file, the column of the event in the metafile and (unless the type is \'regular\') a file with domain-event relations")
 parser.add_argument('--vocab', action='store', required=False, help="[OPTIONAL] give a file with a vocabulary to use for pruning and feature selection")
 parser.add_argument('-c', action='store', required=True, choices=["lcs","knn","ibt","dist"], help="the classifier")
-parser.add_argument('-a', action='store', required=False, nargs='+', help="the arguments needed for the chosen algorithm:\n\n[LCS] specify respectively the directory in which classification is performed (make sure the config file and optionally a data-directory with indexes are present in this directory) and the directory in which files are stored\n[KNN] specify value(s) of k (the classifier will be ran for each value of k)\n[IBT] for the informed baseline time, choose to set the system to dummy by filling in \"dummy\". for dist, specify a file with events.")
+parser.add_argument('-a', action='store', required=False, nargs='+', help="the arguments needed for the chosen algorithm:\n\n[LCS] specify respectively the directory in which classification is performed (make sure the config file and optionally a data-directory with indexes are present in this directory) and the directory in which files are stored\n[KNN] specify value(s) of k (the classifier will be ran for each value of k)\n[IBT] for the informed baseline time, choose to set the system to dummy by filling in \"dummy\"")
 parser.add_argument('-p', action='store', required=False, help="[OPTIONAL] to prune features, give a minimum frequency threshold")
 parser.add_argument('-s', action='store', required=False, help="[OPTIONAL] to select features based on their infogain, specify the number of features to select") 
 parser.add_argument('--tl', action='store_true', required=False, help="[OPTIONAL] set \'tl\' in order to have second stage or hidden timelabel classification")
@@ -58,7 +59,10 @@ if validation=="test":
 
 elif validation=="looe":
     parameters=args.l
-    meta=codecs.open(parameters[1],"r","utf-8")
+    if args.c = "ibt":
+        meta=codecs.open(args.i)
+    else:
+        meta=codecs.open(parameters[1],"r","utf-8")
     metaread=meta.readlines()
     event_column=int(parameters[2])
     event_train_test=defaultdict(lambda : defaultdict(list))
@@ -76,130 +80,143 @@ elif validation=="looe":
             meta = [] 
         meta.append(instance)
 
-    if args.c == "knn" or args.c == "dist":
-        delimiter = ","
-    elif args.c == "lcs":
-        delimiter = " "
-
-    for i,event_bound in enumerate(event_bounds):
-        event=event_bound[0]
-        start=event_bound[1]
-        if i==len(event_bounds)-1:
-            test=[]
-            for i in range(len(instances[start:])):
-                values = instances[start+i].strip().split(delimiter)
-                meta_values = metaread[start+i].strip().split("\t")
-                instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
-                test.append(instance)
-            training=[]
-            for i in range(len(instances[:start])):
-                values = instances[i].strip().split(delimiter)
-                meta_values = metaread[i].strip().split("\t")
-                instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
-                training.append(instance)
-        else:
-            end=event_bounds[i+1][1]
-            test=[]
-            for i in range(len(instances[start:end])):
-                values = instances[start+i].strip().split(delimiter)
-                meta_values = metaread[start+i].strip().split("\t")
-                instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
-                test.append(instance)
-            training=[]
-            for i in range(len(instances)):
-                values = instances[i].strip().split(delimiter)
-                meta_values = metaread[i].strip().split("\t")
-                instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
-                training.append(instance)
-            del training[start:end]
-        event_train_test[event]["training"] = training
-        event_train_test[event]["test"] = test
-        
-    if parameters[0]=="regular":
+    if args.c == "ibt":
+        d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + "baseline/"
         for event in event_train_test.keys():
             print event
             event_write = event
             if re.search(" ",event_write):
                 event_write="_".join(event_write.split(" "))
-            d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + event_write + "/all_to_one/"
-            if not os.path.exists(d):
-                if not os.path.exists("/".join(d.split("/")[:-2])):
-                    if not os.path.exists("/".join(d.split("/")[:-1])):
-                        os.system("mkdir " + "/".join(d.split("/")[:-1]))
-                    os.system("mkdir " + "/".join(d.split("/")[:-2]))
-                os.system("mkdir " + d)
-            if args.parralel:
-                print event
-                print len(event_train_test[event]["meta"])
-                try:
-                    p=multiprocessing.Process(target=classify,args=[event_train_test[event],d])
-                    p.start()
-                except OSError:
-                    classify(event_train_test[event],d)
-            else:
-                classify(event_train_test[event],d)
-    
-    else:
-        domain_events=codecs.open(parameters[3],"r","utf-8")
-        domain_event_hash=defaultdict(list)
-        for line in domain_events.readlines():
-            tokens=line.strip().split("\t")
-            domain_event_hash[tokens[0]].append(tokens[1])
-        domain_events.close()
+            d_event = d + event_write + "/"
+            meta = event_train_test["meta"]
+            informed_baseline_time.ibt(meta,d_event,args[0])
 
-        if parameters[0]=="outer_domain":
-            domains=domain_event_hash.keys()
-            for i,domain in enumerate(domains):
-                print domain
+    else:
+
+        if args.c == "knn":
+            delimiter = ","
+        elif args.c == "lcs":
+            delimiter = " "
+
+        for i,event_bound in enumerate(event_bounds):
+            event=event_bound[0]
+            start=event_bound[1]
+            if i==len(event_bounds)-1:
+                test=[]
+                for i in range(len(instances[start:])):
+                    values = instances[start+i].strip().split(delimiter)
+                    meta_values = metaread[start+i].strip().split("\t")
+                    instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
+                    test.append(instance)
                 training=[]
-                for j in range(len(domains)):
-                    if j != i:
-                        train_domain=domain_event_hash[domains[j]]
-                        for train_event in train_domain:
-                            training.extend(event_train_test[train_event][1])
-                for event in domain_event_hash[domain]:
-                    test=event_train_test[event][1]
-                    if re.search(" ",event):
-                        event="_".join(event.split(" "))
+                for i in range(len(instances[:start])):
+                    values = instances[i].strip().split(delimiter)
+                    meta_values = metaread[i].strip().split("\t")
+                    instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
+                    training.append(instance)
+            else:
+                end=event_bounds[i+1][1]
+                test=[]
+                for i in range(len(instances[start:end])):
+                    values = instances[start+i].strip().split(delimiter)
+                    meta_values = metaread[start+i].strip().split("\t")
+                    instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
+                    test.append(instance)
+                training=[]
+                for i in range(len(instances)):
+                    values = instances[i].strip().split(delimiter)
+                    meta_values = metaread[i].strip().split("\t")
+                    instance = {"features":values[:-1],"label":values[-1],"meta":meta_values}
+                    training.append(instance)
+                del training[start:end]
+            event_train_test[event]["training"] = training
+            event_train_test[event]["test"] = test
+            
+        if parameters[0]=="regular":
+            for event in event_train_test.keys():
+                print event
+                event_write = event
+                if re.search(" ",event_write):
+                    event_write="_".join(event_write.split(" "))
+                d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + event_write + "/all_to_one/"
+                if not os.path.exists(d):
+                    if not os.path.exists("/".join(d.split("/")[:-2])):
+                        if not os.path.exists("/".join(d.split("/")[:-1])):
+                            os.system("mkdir " + "/".join(d.split("/")[:-1]))
+                        os.system("mkdir " + "/".join(d.split("/")[:-2]))
+                    os.system("mkdir " + d)
+                if args.parralel:
                     print event
-                    d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + args.a + "/" + event + "/outer_domain/"
-                    if not os.path.exists(d):
-                        if not os.path.exists("/".join(d.split("/")[:-2])):
-                            if not os.path.exists("/".join(d.split("/")[:-1])):
-                                os.system("mkdir " + "/".join(d.split("/")[:-1]))
-                            os.system("mkdir " + "/".join(d.split("/")[:-2]))
-                        os.system("mkdir " + d) 
-                    if args.parralel:
-                        p=multiprocessing.Process(target=classify,args=[training,test,d])
+                    print len(event_train_test[event]["meta"])
+                    try:
+                        p=multiprocessing.Process(target=classify,args=[event_train_test[event],d])
                         p.start()
-                    else:
-                        classify(training,test,d)
-                        
-        elif parameters[0]=="inner_domain":
-            domains=domain_event_hash.keys()
-            for i,domain in enumerate(domains):
-                print domain
-                for event in domain_event_hash[domain]:
-                    train_events=list(domain_event_hash[domain])
-                    train_events.remove(event)
+                    except OSError:
+                        classify(event_train_test[event],d)
+                else:
+                    classify(event_train_test[event],d)
+        
+        else:
+            domain_events=codecs.open(parameters[3],"r","utf-8")
+            domain_event_hash=defaultdict(list)
+            for line in domain_events.readlines():
+                tokens=line.strip().split("\t")
+                domain_event_hash[tokens[0]].append(tokens[1])
+            domain_events.close()
+
+            if parameters[0]=="outer_domain":
+                domains=domain_event_hash.keys()
+                for i,domain in enumerate(domains):
+                    print domain
                     training=[]
-                    for te in train_events:
-                        training.extend(event_train_test[te][1]) 
-                    test=event_train_test[event][1]
-                    if re.search(" ",event):
-                        event="_".join(event.split(" "))
-                    print event
-                    d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + args.a + "/" + event + "/" + "/" + "inner_domain"
-                    if not os.path.exists(d):
-                        if not os.path.exists("/".join(d.split("/")[:-2])):
-                            if not os.path.exists("/".join(d.split("/")[:-1])):
-                                os.system("mkdir " + "/".join(d.split("/")[:-1]))
-                            os.system("mkdir " + "/".join(d.split("/")[:-2]))
-                        os.system("mkdir " + d) 
-                    
-                    if args.parralel:
+                    for j in range(len(domains)):
+                        if j != i:
+                            train_domain=domain_event_hash[domains[j]]
+                            for train_event in train_domain:
+                                training.extend(event_train_test[train_event][1])
+                    for event in domain_event_hash[domain]:
+                        test=event_train_test[event][1]
+                        if re.search(" ",event):
+                            event="_".join(event.split(" "))
+                        print event
+                        d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + args.a + "/" + event + "/outer_domain/"
+                        if not os.path.exists(d):
+                            if not os.path.exists("/".join(d.split("/")[:-2])):
+                                if not os.path.exists("/".join(d.split("/")[:-1])):
+                                    os.system("mkdir " + "/".join(d.split("/")[:-1]))
+                                os.system("mkdir " + "/".join(d.split("/")[:-2]))
+                            os.system("mkdir " + d) 
+                        if args.parralel:
                             p=multiprocessing.Process(target=classify,args=[training,test,d])
                             p.start()
-                    else:
-                        classify(training,test,d)
+                        else:
+                            classify(training,test,d)
+                            
+            elif parameters[0]=="inner_domain":
+                domains=domain_event_hash.keys()
+                for i,domain in enumerate(domains):
+                    print domain
+                    for event in domain_event_hash[domain]:
+                        train_events=list(domain_event_hash[domain])
+                        train_events.remove(event)
+                        training=[]
+                        for te in train_events:
+                            training.extend(event_train_test[te][1]) 
+                        test=event_train_test[event][1]
+                        if re.search(" ",event):
+                            event="_".join(event.split(" "))
+                        print event
+                        d="/".join(args.i.split(".txt")[0].split("/")[:-1]) + "/" + args.a + "/" + event + "/" + "/" + "inner_domain"
+                        if not os.path.exists(d):
+                            if not os.path.exists("/".join(d.split("/")[:-2])):
+                                if not os.path.exists("/".join(d.split("/")[:-1])):
+                                    os.system("mkdir " + "/".join(d.split("/")[:-1]))
+                                os.system("mkdir " + "/".join(d.split("/")[:-2]))
+                            os.system("mkdir " + d) 
+                        
+                        if args.parralel:
+                                p=multiprocessing.Process(target=classify,args=[training,test,d])
+                                p.start()
+                        else:
+                            classify(training,test,d)
 
