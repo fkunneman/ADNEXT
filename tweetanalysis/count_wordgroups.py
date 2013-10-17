822b6f17f58b084d4ce296e8cdd198990425af62
Florian Kunneman <f.kunneman@let.ru.nl>
2013-10-17 10:23:26 +0200

    finished

--- tweetanalysis/count_wordgroups.py
+++ tweetanalysis/count_wordgroups.py
@@ -11,15 +11,34 @@ partsfile = sys.argv[1]
 files_directory = sys.argv[2]
 wordgroups = sys.argv[3]
 outfile = open(sys.argv[4],"a")
+hashtag = sys.argv[5]
 
 #generate per-file featuresets
-#feature_sets = gen_functions.read_lcs_files(partsfile,files_directory)
+feature_sets = gen_functions.read_lcs_files(partsfile,files_directory)
 
 #generate feature-frequency hash
-#feature_frequency = defaultdict(int)
-#for features in feature_sets:
-#    for feature in features:
-#	        feature_frequency[feature] += 1
+feature_frequency = defaultdict(int)
+for features in feature_sets:
+    for feature in features:
+	        feature_frequency[feature] += 1
 
-#generate word groups
-gen_functions.excel2columns(wordgroups)
+#generate word-category pairs
+word_cat = gen_functions.excel2columns(wordgroups)
+top_features = word_cat.keys()
+
+shares = defaultdict(int)
+total = 0
+for feature in feature_frequency.keys():
+    frequency = feature_frequency[feature]
+    if feature in top_features:
+        shares[word_cat[feature]] += frequency
+    else: 
+        shares["remainder"] += frequency
+    total += frequency
+
+outfile.write("\npercentage for " + hashtag + ":\n")
+for cat in shares.keys():
+    outfile.write(cat + ":\t" + str(shares[cat]))
+
+outfile.write("\n")
+outfile.close()
