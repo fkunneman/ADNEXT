ddfb4183c482895f04b522ac0c602e61b6290878
Florian Kunneman <f.kunneman@let.ru.nl>
2013-10-22 11:59:59 +0200

    timelabels

--- tweetprocessing/timelabel_instances.py
+++ tweetprocessing/timelabel_instances.py
@@ -1,73 +1,46 @@
-    if args.tl:
-    time_unit = time_labels[0]
-    label_type = time_labels[1]
-    if len(time_labels) > 2:
-        tf.generate_time_label(time_unit,label_type,int(time_labels[2]))
+#!/usr/bin/env python
+
+import argparse
+import codecs
+import time_functions
+from collections import defaultdict
+import datetime
+
+"""
+Script to put metadata in the right order for tweets_2_features.
+Presumes file for 1 event
+"""
+parser = argparse.ArgumentParser(description = "Script to put metadata in the right order for tweets_2_features.")
+parser.add_argument('-i', action = 'store', required = True, help = "the input file")  
+parser.add_argument('-o', action = 'store', required = True, help = "the output file")
+parser.add_argument('-d', action = 'store', type=int, required = True, help = "field with the tweet date")
+parser.add_argument('-t', action = 'store', type=int, required = True, help = "field with the tweet time")
+parser.add_argument('-e', action = 'store', required = True, help = "file with event-time information")
+parser.add_argument('-n', action = 'store', required = True, help = "event name")
+parser.add_argument('-u', action = 'store', choices = ["day","hour","minute"], default='day', help = "specify the unit of time for the timelabel")
+parser.add_argument('-b', action = 'store', type=int, required = False, help = "for the inclusion of an early" + 
+    "class, specify before which anount of time units this label is given")
+args = parser.parse_args() 
+
+tweetfile = codecs.open(args.i,"utf-8","r")
+outfile = codecs.open(args.o,"utf-8","r")
+
+eventhash = time_functions.generate_event_time_hash(args.e)
+event_time = eventhash[args.n]
+for tweet in tweetfile.readlines():
+    tokens = tweet.split("\t")
+    time = time_functions.return_datetime(tokens[args.d],tokens[args.t],"vs")
+    #Extract the time difference between the tweet and the event 
+    if tweet_datetime < event_datetime_begin:
+        tweet_event_time=time_functions.timerel(event_datetime_begin,tweet_datetime,timeunit) * -1
+        if args.b and tweet_event_time < args.b:
+            label="early"
+        else:
+            label=str(tweet_event_time)
     else:
-        tf.generate_time_label(time_unit,label_type)
-
-    # def generate_tfz(self,agg=False):
-    #     """
-    #     Add time-from zero in days information to tweets based on their time of posting
-    #     """
-    #     event_tweets=defaultdict(list)
-    #     event_tweets_tfz=defaultdict(list)
-    #     for t in self.instances:
-    #         event_tweets[t.event].append(t)
-        
-    #     for event in event_tweets.keys():
-    #         tweets=event_tweets[event]
-    #         tweets_tfz=[]
-    #         tweets.sort(key=operator.methodcaller("get_datetime"), reverse=False)
-    #         zeropoint=tweets[0].get_datetime()
-    #         for t in tweets:
-    #             tfz=int(time_functions.timerel(t.get_datetime(),zeropoint,"day"))
-    #             t.tfz=str(tfz)
-    #             tweets_tfz.append(([t],tfz))
-    #             print t.get_datetime(),tfz        
-    #         if agg:
-    #             window=int(agg[0])
-    #             slider=int(agg[1])
-    #             event_tweets_tfz[event]=time_functions.extract_sliding_window_instances(tweets_tfz,window,slider)                    
-    #         else: 
-    #             event_tweets_tfz[event]=tweets_tfz
-            
-    #     return event_tweets_tfz
-
-    def generate_time_label(self,timeunit,labeltype,threshold=False):
-        """
-        Add a label to tweets based on their tte
-        The unit of time given as a label can be 'minute', 'hour' or 'day'.
-        """
-        threshold=threshold * -1
-        for t in self.instances:
-            #Get the time of the event mentioned in the tweet 
-            tweet_datetime=time_functions.return_datetime(t.date,t.time,"vs")
-            event_datetime_begin=self.eventhash[t.event][0]
-            event_datetime_end=self.eventhash[t.event][1]
-            #Extract the time difference between the tweet and the event 
-            if tweet_datetime < event_datetime_begin:
-                tweet_event_time=time_functions.timerel(event_datetime_begin,tweet_datetime,timeunit) * -1
-                if labeltype=="category":
-                    t.label="before"
-                    if threshold and tweet_event_time < threshold:
-                        t.tte="early"
-                    else:
-                        t.tte=str(tweet_event_time)
-                else:
-                    if threshold and tweet_event_time < threshold:
-                        t.label="early"
-                        t.tte="early"
-                    else:
-                        t.label=str(tweet_event_time)
-                        t.tte=str(tweet_event_time)
-            else:
-                if labeltype != "before":
-                    if tweet_datetime < event_datetime_end:
-                        t.set_label("during")
-                    else:
-                        if labeltype=="category":
-                            t.set_label("after")
-                        else:
-                            tweet_event_time=time_functions.timerel(event_datetime_begin,tweet_datetime,"minute")
-                            t.set_label=tweet_event_time
+        if tweet_datetime < event_datetime_end:
+            label="during"
+        else:
+            label="after"
+    tokens = label + tokens
+    outfile.write("\t".join(tokens))
