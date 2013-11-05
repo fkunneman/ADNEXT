0218895a0b3f1a4059ce8e4695ab14e425803ddf
Florian Kunneman <f.kunneman@gmail.com>
2013-11-05 09:36:56 +0100

    e?

--- classification/classify_events.py
+++ classification/classify_events.py
@@ -75,7 +75,7 @@ for i,event in enumerate(events):
     os.system("mkdir " + eventdir)
     cl = Classifier(train,test,directory = eventdir)
     if args.f:
-        cl.top_features[args.f]
+        cl.top_features(args.f)
     cl.index_features()
     cl.scale_features()
     #cl.perform_svm(args.f)
