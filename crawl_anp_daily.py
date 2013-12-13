5b2a1cecb62f19497f40c78a3c26ed65aebf82f0
Florian Kunneman <f.kunneman@let.ru.nl>
2013-12-13 10:21:14 +0100

    dkal

--- crawl_anp_daily.py
+++ crawl_anp_daily.py
@@ -9,5 +9,6 @@ outdir_files = sys.argv[2]
 
 t = date.today()
 cur = "-".join([str(t.day),str(t.month),str(t.year)])
+cur_out = str(t.year) + "-" + str(t.month) + "-" + str(t.day)
 
-os.system("wget -v --user=f.kunneman@let.ru.nl --password=crawl2013 -w 0.1 -i http://portal.anp.nl/rss/indexer.do?action=importview\&sourceId=1\&day=" + cur + " -o " + outdir_logs + cd + " -O " + outdir_files + cd)
+os.system("wget -v --user=f.kunneman@let.ru.nl --password=crawl2013 -w 0.1 -i http://portal.anp.nl/rss/indexer.do?action=importview\&sourceId=1\&day=" + cur + " -o " + outdir_logs + cur_out + " -O " + outdir_files + cur_out)
