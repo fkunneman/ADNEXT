1a64d67f0c022d7420cd864043a14a1b52ae2ce6
Florian Kunneman <f.kunneman@gmail.com>
2013-12-16 14:15:28 +0100

    d

--- tweetprocessing/timetag_tweets.py
+++ tweetprocessing/timetag_tweets.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 parser = argparse.ArgumentParser(description = "Program that can be used to change or make additions to any file with (possibly column-based) lines with a consistent format")
 parser.add_argument('-i', action = 'store', required = True, help = "The input file.")  
 parser.add_argument('-o', action = 'store', required = True, help = "The output directory.")
-parser.add_argument('-h', action = 'store', required = True, help = "The heideltime directory.")
+parser.add_argument('-w', action = 'store', required = True, help = "The heideltime directory.")
 parser.add_argument('-d', action = 'store', type = int, required = True, help = "Specify the column with a date.")
 # parser.add_argument('-a', action = 'store', required = False, choices = ["add","replace","delete","delete_filematch","extract","add_time","add_id","filter"], help = "Choose the action to perform.")
 # parser.add_argument('-s', action = 'store', required = False, help = "give a string as argument for add, replace, delete or filter")
@@ -31,13 +31,13 @@ for line in infile.readlines():
 for date in date_tweets.keys():
     dateout = open(outdir_date + date,"w")
     date_file[date] = dateout
-    for tweet in date_tweets[date]
+    for tweet in date_tweets[date]:
         dateout.write(tweet)
     dateout.close()
 
 outdir_tags = args.o + "dates_tagged/"
 os.chdir(args.h)
 for date in date_file.keys():
-    tagged_out = outdir_tags + date)
+    tagged_out = outdir_tags + date
     os.system("java -jar de.unihd.dbs.heideltime.standalone.jar " + date_file[date] + " -l DUTCH -t NEWS -dct " + date + " > " + tagged_out)
 
