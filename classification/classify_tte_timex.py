f92725d7b749d50cc0364f0bbb222bf83be5c43b
Florian Kunneman <f.kunneman@let.ru.nl>
2013-12-17 15:57:15 +0100

    fda

--- classification/classify_tte_timex.py
+++ classification/classify_tte_timex.py
@@ -8,7 +8,7 @@ import codecs
 
 parser=argparse.ArgumentParser(description="Program to perform a classification experiment with time-tagged event tweets in a sliding window fashion")
 parser.add_argument('-t', action='store', required=True, help="the directory with time-tagged tweets")
-parser.add_argument('-i', action='store', help="the file with ordered tweets in time")
+parser.add_argument('-i', action='store', help="the directory with meta-tagged tweets")
 parser.add_argument('-d', action='store', type=int, help="the date-column in the ordered tweetfile")
 parser.add_argument('--step', action='store', default=1, type=int, help="specify the stepsize of instance windows; [DEFAULT] = 1")
 parser.add_argument('--window', action='store', default=100, type=int, help="specify the size of instance windows; [DEFAULT] = 100")
@@ -19,6 +19,7 @@ args = parser.parse_args()
 
 #extract tweets from datefile and make a date-tweets dict
 date_tweets = defaultdict(list)
+ordered_tweets = []
 for f in listdir(args.t):
     date = re.sub(".txt","",f)
     datefile = codecs.open(args.t + f,"r","utf-8")
@@ -33,16 +34,19 @@ for f in listdir(args.t):
                 within = True
     datefile.close()
 
-ordered_tweets = codecs.open(args.i,"r","utf-8")
-ordered_tweets_timex = []
-for line in ordered_tweets.readlines():
-    instance = {}
-    tokens = line.strip().split("\t")
-    date = tokens[args.d]
-    tweet = tokens[-1]
-    if not re.search("RT",tweet):
-        tagged_tweet = date_tweets[date].pop(0)
-        print tagged_tweet
+print sorted(date_tweets.keys(),reverse=True)
+
+# exit()
+
+# ordered_tweets_timex = []
+# for line in ordered_tweets.readlines():
+#     instance = {}
+#     tokens = line.strip().split("\t")
+#     date = tokens[args.d]
+#     tweet = tokens[-1]
+#     if not re.search("RT",tweet):
+#         tagged_tweet = date_tweets[date].pop(0)
+#         print tagged_tweet
 
 
 
