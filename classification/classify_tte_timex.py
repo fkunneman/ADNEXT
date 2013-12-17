44c0813fe51ffe5a692d477b783cf0c30fe9462a
Florian Kunneman <f.kunneman@let.ru.nl>
2013-12-17 15:32:02 +0100

    fdafdsa

--- classification/classify_tte_timex.py
+++ classification/classify_tte_timex.py
@@ -9,8 +9,10 @@ import codecs
 parser=argparse.ArgumentParser(description="Program to perform a classification experiment with time-tagged event tweets in a sliding window fashion")
 parser.add_argument('-t', action='store', required=True, help="the directory with time-tagged tweets")
 parser.add_argument('-i', action='store', help="the file with ordered tweets in time")
+parser.add_argument('-d', action='store', type=int, help="the date-column in the ordered tweetfile")
 parser.add_argument('--step', action='store', default=1, type=int, help="specify the stepsize of instance windows; [DEFAULT] = 1")
 parser.add_argument('--window', action='store', default=100, type=int, help="specify the size of instance windows; [DEFAULT] = 100")
+
 args = parser.parse_args()
 
 #generate ordered list of timetagged tweets
@@ -29,8 +31,20 @@ for f in listdir(args.t):
         else:
             if re.search("<TimeML>",line):
                 within = True
+    datefile.close()
 
-print date_tweets
+ordered_tweets = codecs.open(args.i,"r","utf-8")
+ordered_tweets_timex = []
+for line in ordered_tweets.readlines():
+    instance = {}
+    tokens = line.strip().split("\t")
+    date = tokens[args.d]
+    tweet = date_tweet[date].pop(0)
+    print tweet
+
+
+
+
 
 
 #generate windows from the list
