851c767fcd5db022315fc69cc0bf01ed1e1f0fe9
Florian Kunneman <f.kunneman@gmail.com>
2013-12-24 14:24:58 +0100

    fdafao

--- classification/linear_regression_event.py
+++ classification/linear_regression_event.py
@@ -2,6 +2,7 @@
 
 import argparse
 from collections import defaultdict
+import os
 import math
 from itertools import chain
 import numpy
@@ -11,7 +12,7 @@ Script to perform simple linear regression adapted to tweet distributions for an
 """
 parser=argparse.ArgumentParser(description="Script to perform simple linear regression adapted to tweet distributions for an event")
 parser.add_argument('-i', action='store', nargs = '+', required=True, help="files with event tweets")
-parser.add_argument('-l', action='store', type = int, default = 0, help="specify the column with the time-to-event label" )
+parser.add_argument('-l', action='store', default = 0, help="specify the column with the time-to-event label" )
 parser.add_argument('-n', action='store', type = int, default=10, help="specify n for n-fold")
 parser.add_argument('-d', action='store', help = "the output directory")
 
@@ -23,14 +24,15 @@ for ef in args.i:
     instance_file = open(ef)
     instances_raw=instance_file.readlines()
     instance_file.close()
-    event = "/".join(ef.split("/")[-1])
+    event = "/".join(ef.split("/")[-2:])
     tte_tweets = defaultdict(int)
     for tweet in instances_raw:
         values = tweet.strip().split("\t")
         tte = values[args.l]
-        tte_tweets[tte] += 1
+        if not tte == "after" and not tte == "during":
+            tte_tweets[int(tte)] += 1
     #make pairs of the time_to_event and the (logged) number of tweets in this period 
-    buckets = [(tte,math.log(tte_tweets[tte],2)) for tte in tte_tweets.keys()]
+    buckets = [(tte,math.log(tte_tweets[tte]) / math.log(10)) for tte in tte_tweets.keys()]
     event_buckets[event] = buckets       
 
 #for each train-test
@@ -50,9 +52,16 @@ for i in range(0,len(events),testlen):
     model = numpy.polyfit(valuevect,targetvect,1)
     #predict tte
     for event in test_events:
+        eventdir = "/".join((args.d + event).split("/")[:-1]) + "/"
+        if not os.path.exists(eventdir):
+            d = -2
+            while d <= -1: 
+                if not os.path.exists("/".join(eventdir.split("/")[:d])):
+                    os.system("mkdir " + "/".join(eventdir.split("/")[:d]))
+                d += 1
         eventout = open(args.d + event,"w")
         targets = [t[0] for t in event_buckets[event]]
         values = [t[1] for t in event_buckets[event]]
-        for j,value in enumerate(values):
-            estimation = int(round((model[0]*value) + model[1],0))
-            eventout.write("\t".join([str(targets[j]),str(estimation)]) + "\n")
+        for j,target in enumerate(sorted(targets)):
+            estimation = int(round((model[0]*values[j]) + model[1],0))
+            eventout.write("\t".join([str(target),str(estimation)]) + "\n")
