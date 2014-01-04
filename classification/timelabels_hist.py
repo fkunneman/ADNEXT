e46ce47ef9ac2a4888d3d2953298dc4b2036ae06
Florian Kunneman <f.kunneman@let.ru.nl>
2014-01-04 14:03:11 +0100

    fdaf

---
+++ classification/timelabels_hist.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import argparse
+import evalset
+import re
+
+parser = argparse.ArgumentParser(description = "Program to add historic knowledge to time-to-event estimations")
+
+parser.add_argument('-i', action = 'store', required = True, help = "the files with estimations")
+parser.add_argument('-r', action='store', required=True, help = "the file to write new estimations to")
+parser.add_argument('-t', action='store', type = int, default = 0, help = "the target column")
+parser.add_argument('-c', action='store', type = int, default = 1, help = "the estimation column")
+parser.add_argument('-s', action='store', type = int, default = 0, help = "specify the first line of classification files to process from [DEFAULT = 0]")
+parser.add_argument('-d', action = 'store', default = "\t", help="specify the delimiter of target and estimator columns [DEFAULT = \'\\t\']")
+
+args = parser.parse_args()
+
+outfile = open(args.r,"w")
+
+estimations_file = open(args.i)
+weights = defaultdict(float)
+window_weights = []
+for estimation in estimations_file.readlines()[args.s:]:
+    tokens = estimation.strip().split(args.d)
+    target = tokens[args.t]
+    classification = tokens[args.c]
+    weights[classification-target] += 1
+    window_weights.append((target,weights.copy()))
+estimations_file.close()
+# return RMSE, responsiveness and prediction@
+print window_weights
+
+# es = evalset.Evalset()
+# es.add_instances(event_estimations)
+# rmse = es.calculate_rmse()
+# rmses.append(rmse)
+# outfile.write("\t".join([event,str(rmse[0]),str(rmse[1])]) + "\n")
+# # write to file and keeplist  
+
+# rmse_all,responsiveness_all = zip(*rmses)
+# rmse_mean = str(sum(rmse_all) / len(rmse_all))
+# responsiveness_mean = str(sum(responsiveness_all) / len(responsiveness_all))
+# outfile.write("\t".join(["mean",rmse_mean,responsiveness_mean]) + "\n")
+# outfile.close()
