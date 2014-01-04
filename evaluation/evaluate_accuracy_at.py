8f5db2674eedd7f3719ce6b44adba1f2eb00873c
Florian Kunneman <f.kunneman@let.ru.nl>
2014-01-04 20:53:06 +0100

    fdaf

--- evaluation/evaluate_accuracy_at.py
+++ evaluation/evaluate_accuracy_at.py
@@ -13,7 +13,7 @@ parser.add_argument('-t', action='store', type = int, default = 0, help = "the t
 parser.add_argument('-c', action='store', type = int, default = 1, help = "the estimation column")
 parser.add_argument('-o', action='store', type = int, required=False, help = "[OPTIONAL] the score column")
 parser.add_argument('-a', action='store', choices = ["estimation","threshold"], help = "the domain of the stop condition (estimation or threshold)")
-parser.add_argument('-v', action='store', type = int, required = True, help = "the value at which to stop")
+parser.add_argument('-v', action='store', type = int, nargs='+', required = True, help = "the value(s) at which to stop")
 parser.add_argument('-s', action='store', type = int, default = 0, help = "specify the first line of classification files to process from [DEFAULT = 0]")
 parser.add_argument('-d', action = 'store', default = "\t", help="specify the delimiter of target and estimator columns [DEFAULT = \'\\t\']")
 parser.add_argument('--depth', action='store', default=1, type=int, help="specify the depth of file characterizations; [DEFAULT = 1]")
@@ -22,8 +22,8 @@ args = parser.parse_args()
 
 depth = args.depth * -1
 outfile = open(args.r,"w")
-outfile.write("\n")
-aats = []
+outfile.write(args.a + "\n")
+aats = defaultdict(list)
 # for each file
 for ef in args.i:
     # extract event + windowname
@@ -45,13 +45,19 @@ for ef in args.i:
     # return RMSE, responsiveness and prediction@
     es = evalset.Evalset()
     es.add_instances(event_estimations)
-    aat = es.accuracy_at(args.v,args,a)
-    aats.append(aat)
-    outfile.write("\t".join([event,str(aat[0]),str(aat[1]),str(aat[2])]) + "\n") 
+    outfile.write(event + "\n\t")
+    for v in args.v:
+        aat = es.accuracy_at(v,args.a)
+        aats[v].append(aat)
+        outfile.write(" ".join([str(v),str(aat[0]),str(aat[1]),str(aat[2])]) + "\t") 
+    outfile.write("\n")
 
-timeat_all,prediction_all,dif_all = zip(*rmses)
-timeat_mean = str(sum(timeat_all) / len(timeat_all))
-prediction_mean = str(sum(prediction_all) / len(prediction_all))
-dif_mean = str(sum(dif_all) / len(dif_all))
-outfile.write("\t".join(["mean",timeat_mean,prediction_mean,dif_mean]) + "\n")
+outfile.write("\nMeans:\n")
+for v in args.v:
+    outfile.write("scores for prediction at " + args.a + " " + str(v) + "\n")
+    timeat_all,prediction_all,dif_all = zip(*aats[v])
+    timeat_mean = str(sum(timeat_all) / len(timeat_all))
+    prediction_mean = str(sum(prediction_all) / len(prediction_all))
+    dif_mean = str(sum(dif_all) / len(dif_all))
+    outfile.write(" ".join([str(v),str(timeat_mean),str(prediction_mean),str(dif_mean)]) + "\n")
 outfile.close()
