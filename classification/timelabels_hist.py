36ee92608a38271751534f58b001e3bc09725fcf
Florian Kunneman <f.kunneman@gmail.com>
2014-01-04 14:22:48 +0100

    gf

--- classification/timelabels_hist.py
+++ classification/timelabels_hist.py
@@ -17,7 +17,7 @@ args = parser.parse_args()
 outfile = open(args.r,"w")
 
 estimations_file = open(args.i)
-weights = defaultdict(float)
+weights = defaultdict(int)
 window_weights = []
 for estimation in estimations_file.readlines()[args.s:]:
     tokens = estimation.strip().split(args.d)
@@ -31,24 +31,16 @@ for estimation in estimations_file.readlines()[args.s:]:
         weights[classification] += 1
     window_weights.append((target,classification,weights.copy()))
 estimations_file.close()
-# return RMSE, responsiveness and prediction@
+during = False
 for ww in window_weights:
     t = ww[0]
+    if t == "during":
+        during = True
     c = ww[1]
-    d = ww[2]
-    topest = [(e,d[e]) for e in sorted(d, key=d.get, reverse=True)[0]]
-    est = t + topest
-    print t,c,topest,est 
-
-# es = evalset.Evalset()
-# es.add_instances(event_estimations)
-# rmse = es.calculate_rmse()
-# rmses.append(rmse)
-# outfile.write("\t".join([event,str(rmse[0]),str(rmse[1])]) + "\n")
-# # write to file and keeplist  
-
-# rmse_all,responsiveness_all = zip(*rmses)
-# rmse_mean = str(sum(rmse_all) / len(rmse_all))
-# responsiveness_mean = str(sum(responsiveness_all) / len(responsiveness_all))
-# outfile.write("\t".join(["mean",rmse_mean,responsiveness_mean]) + "\n")
-# outfile.close()
+    if during:
+        print t,c
+    else:
+        d = ww[2]
+        topest = [e for e in sorted(d, key=d.get, reverse=True)][0]
+        est = t + topest
+        print t,c,topest,est 
