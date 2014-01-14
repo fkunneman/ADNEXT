045327a7e293ecc2706b5605069733765147b842
Florian Kunneman <f.kunneman@let.ru.nl>
2014-01-14 14:14:02 +0100

    fdaf

--- sample_tweets.py
+++ sample_tweets.py
@@ -3,12 +3,18 @@
 import argparse
 import gzip
 import codecs
+import random
+from collections import defaultdict
+
+import time_functions
 
 parser = argparse.ArgumentParser(description = "Program to sample Dutch tweets from an extended \
     period")
 
 parser.add_argument('-i', action = 'store', nargs = '+', required = True, 
     help = "the input files")
+parser.add_argument('-o', action = 'store', required = True,
+    help = "the output file")
 parser.add_argument('-n', action = 'store', type = int, required = True, 
     help = "the number of tweets to sample")
 parser.add_argument('-t', action = 'store_true', required = True, 
@@ -26,16 +32,33 @@ for i in args.i:
         infile = codecs.open(i,"r","utf-8")
     tweets.extend(infile.readlines())
     infile.close
-    print len(tweets)
 
-#count tweets
-
-
-
+outfile = codecs.open(args.o,"w","utf-8")
 #if smooth: 
-#   make time dict of day-hours
-#   sample 1/24 from each hour
+if args.t:
+    #make time dict of day-hours
+    hour_tweets = defaultdict(list)
+    for tweet in tweets:
+        dt = time_functions.return_datetime(time=True,setting="vs")
+        hour_tweets[dt.hour].append(tweet)
 
+    percent = args.n / len(tweets)
+    remains = len(tweets)
+    for i,hour in enumerate(hour_tweets.keys()):
+        htweets = hour_tweets[hour]
+        if i == 23:
+            extract = remains
+        else:
+            extract = int(round(percent * len(htweets),0))
+            remains -= extract
+        sample = random.sample(htweets, extract)
+        for line in sample:
+            outfile.write(line)
 
-# else:
-# sample randomly from all
+# else sample randomly from all
+else:
+    sample = random.sample(tweets,args.n)
+    for line in sample:
+        outfile.write(line)
+
+outfile.close()
