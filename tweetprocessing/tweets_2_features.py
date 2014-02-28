96b47ee861eaaadf42b3928d46d2fad5a09ed5ef
Florian Kunneman <f.kunneman@let.ru.nl>
2014-02-28 11:09:25 +0100

    fix

--- tweetprocessing/tweets_2_features.py
+++ tweetprocessing/tweets_2_features.py
@@ -56,7 +56,11 @@ if args.e:
     #generate list
     extractfile = codecs.open(args.e,"r","utf-8")
     extracts = extractfile.readlines()
-    tf.extract_listfeatures(extracts)
+    print extracts
+    ep = extractfile.read().split("\n")
+    print ep
+    #tf.extract_listfeatures(extracts)
+    extractfile.close()
 if args.n:
     for n in args.n:
         tf.add_ngrams(n=int(n))
