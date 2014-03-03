a6c243542ebf463bce919a8c773f0b03315e9c5e
Florian Kunneman <f.kunneman@let.ru.nl>
2014-03-03 13:16:01 +0100

    fix

--- tweetprocessing/tweetsfeatures.py
+++ tweetprocessing/tweetsfeatures.py
@@ -90,7 +90,7 @@ class Tweetsfeatures():
 
     def extract_timefeatures(self):
         days = re.compile(r"(met ?(over|nog( slechts)?)( maar| een kleine| pakweg| iets (meer|minder) dan)? )(\d+) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)( slapen)?( tot)?",re.IGNORECASE)
-        days2 = re.compile(r"(\d+) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)( slapen)?( tot)",re.IGNORECASE))
+        days2 = re.compile(r"(\d+) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)( slapen)?( tot)",re.IGNORECASE)
         dates = re.compile(r"([1-31] (jan|januari|feb|februari|maa|mrt|maart|apr|april|mei|jun|juni|jul|juli|aug|augustus|sep|september|okt|oktober|nov|november|dec|december))",re.IGNORECASE)
         for instance in self.instances:
             ws = " ".join(instance.wordsequence)
