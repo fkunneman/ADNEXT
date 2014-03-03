bb99743a5ce2994466d1b89d19746fea66795620
Florian Kunneman <f.kunneman@let.ru.nl>
2014-03-03 14:02:23 +0100

    update

--- tweetprocessing/tweetsfeatures.py
+++ tweetprocessing/tweetsfeatures.py
@@ -89,53 +89,57 @@ class Tweetsfeatures():
             t.features.extend(features)
 
     def extract_timefeatures(self):
+        convert_nums = {een:1, twee:2, drie:3, vier:4, vijf:5, zes:6, zeven:7, acht:8, negen:9, tien:10, elf:11, twaalf:12, dertien:13, veertien:14, vijftien:15, zestien:16, zeventien:17, achtien:18, negentien:19, twintig:20}
+        convert_tu = {dagen:1, daagjes:1, nachten:1, nachtjes:1, week: 7, weken:7, weekjes:7, maand: 30, maanden:30, maandjes:30}
         # check = re.compile(r"\b(dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)\b",re.IGNORECASE)
-        days = re.compile(r"over iets (meer|minder) dan (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days1 = re.compile(r"(over|nog) pakweg (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days2 = re.compile(r"nog slechts (een kleine )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days3 = re.compile(r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes) tot",re.IGNORECASE)
+        days = re.compile(r"over iets (meer|minder) dan (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days1 = re.compile(r"(over|nog) pakweg (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days2 = re.compile(r"nog slechts (een kleine )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days3 = re.compile(r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand) tot",re.IGNORECASE)
         days4 = re.compile(r"met nog een kleine (week|maand)",re.IGNORECASE)
-        days5 = re.compile(r"(met )?nog (een kleine )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days6 = re.compile(r"nog (maar )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days7 = re.compile(r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)( slapen)? tot",re.IGNORECASE)
-        days8 = re.compile(r"over (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
-        days9 = re.compile(r"over een kleine (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)",re.IGNORECASE)
+        days5 = re.compile(r"(met )?nog (een kleine )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days6 = re.compile(r"nog (maar )?(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days7 = re.compile(r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)( slapen)? tot",re.IGNORECASE)
+        days8 = re.compile(r"over (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
+        days9 = re.compile(r"over een kleine (\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|veertien|vijftien|zestien|zeventien|achtien|negentien|twintig) (dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes|week|maand)",re.IGNORECASE)
         
         for instance in self.instances:
             ws = " ".join(instance.wordsequence)
+            m = False
             # if check.search(ws):
             #     sh = check.search(ws)
             #     print "check",ws,sh.groups()
             if days.search(ws):
                 sh = days.search(ws)
-                print sh.groups()[:2]
+                m = True
+                # for unit in sh.groups():
+                # print sh.groups()[:2]
             elif days1.search(ws):
                 sh = days1.search(ws)
-                print sh.groups()[:2]
             elif days2.search(ws):
                 sh = days2.search(ws)
-                print sh.groups()[:2]
             elif days3.search(ws):
                 sh = days3.search(ws)
-                print sh.groups()[:2]
             elif days4.search(ws):
                 sh = days4.search(ws)
-                print sh.groups()[:2]
             elif days5.search(ws):
                 sh = days5.search(ws)
-                print sh.groups()[:2]
             elif days6.search(ws):
                 sh = days6.search(ws)
-                print sh.groups()[:2]
             elif days7.search(ws):
                 sh = days7.search(ws)
-                print sh.groups()[:2]
             elif days8.search(ws):
                 sh = days8.search(ws)
-                print sh.groups()[:2]
             elif days9.search(ws):
                 sh = days9.search(ws)
-                print sh.groups()[:2]
+            if m:
+                for unit in sh.groups():
+                    if unit in convert_nums.keys():
+                        num = convert_nums[unit]
+                    elif unit in convert_tu.keys():
+                        tu = convert_tu[unit]
+                feature = num * tu
+                print sh.groups(),feature  
         quit()
 
 
