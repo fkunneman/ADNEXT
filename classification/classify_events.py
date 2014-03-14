9f782ac7498fee492d7c7d228d8885bcdc8f4d51
Florian Kunneman <f.kunneman@let.ru.nl>
2014-03-14 11:45:49 +0100

    upd

--- classification/classify_events.py
+++ classification/classify_events.py
@@ -111,17 +111,17 @@ for i in range(0,len(events),testlen):
                             feature_tte[feature].append(int(tweet["label"]))
                         except:
                             continue
-                        print feature,feature_tte[feature]
-        quit()
         #calculate_median
         print "calculating median"
         feature_new = {}
         for feature in feature_tte.keys():
             if gen_functions.return_standard_deviation(feature_tte[feature]) < 2 and len(feature_tte[feature]) >= 2:
-                feature_new[feature] = str(abs(int(numpy.median(feature_tte[feature])))) + "_days"
+                feature_new[feature] = str(int(numpy.median(feature_tte[feature])))) + "_days"
+                print "yes",feature,feature_tte[feature],str(int(numpy.median(feature_tte[feature])))) + "_days"
             else:
                 feature_new[feature] = feature
-            print feature,feature_tte[feature],feature_new[feature],gen_functions.return_standard_deviation(feature_tte[feature])
+                print "no", feature, feature_tte[feature]
+            #print feature,feature_tte[feature],feature_new[feature],gen_functions.return_standard_deviation(feature_tte[feature])
         quit()
         #convert features
         print "converting features"
