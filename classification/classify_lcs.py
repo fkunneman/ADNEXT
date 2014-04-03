a0dc828082b5686ef0497523a697fe175a8d5d37
Florian Kunneman <f.kunneman@let.ru.nl>
2014-04-03 15:54:56 +0200

    update

--- classification/classify_lcs.py
+++ classification/classify_lcs.py
@@ -52,6 +52,40 @@ for key in config_order:
 if args.t:
     os.system("cp " + args.p + " " + args.d + "/train")
     os.system("cp " + args.t + " " + args.d + "/test") 
+else:
+    folds = []
+    partsopen = open(args.p)
+    instances = partsopen.readlines():
+    partsopen.close()
+    for i in [x+1 for x in range(10)]:
+        ind = 1
+        fold = []
+        while i * ind < len(instances):
+            fold.append(instances[i*ind])
+            ind += 1
+        folds.append(fold)
+    for fold in folds:
+        print "".join(fold)
+
+    # size = len(sorted_instances)
+    # for i in range(n):
+    #     fold_dir = main_dir + "fold_" + str(i) + "/"
+    #     os.system("mkdir " + fold_dir)
+    #     print fold_dir
+    #     train_test = defaultdict(list)
+    #     train_test["training"] = list(sorted_instances)
+    #     j = i
+    #     offset = 0
+    #     while j < size:
+    #         train_test["test"].append(sorted_instances[j])
+    #         #print i,j-offset,len(train_test["train"]),j,size,len(sorted_instances)
+    #         del train_test["training"][j-offset]
+    #         j + =  n
+    #         offset + =  1
+    #     train_test["meta"] = []
+    #     classify(train_test,fold_dir)
+
+quit()
 
 #classify
 os.chdir(args.d)
