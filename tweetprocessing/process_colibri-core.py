5e000537ac4635bca3c5effb626f3066f5b0bc07
Florian Kunneman <f.kunneman@let.ru.nl>
2014-04-21 14:43:46 +0200

    update

---
+++ tweetprocessing/process_colibri-core.py
@@ -0,0 +1,24 @@
+
+import sys
+import os
+import re
+
+textcol = int(sys.argv[1])
+recurse = int(sys.argv[2]) * -1
+outdir = sys.argv[3]
+infiles = sys.argv[4:]
+
+for infile in infiles:
+    parts = infile.split("/")
+    ident = parts[recurse:]
+    eventdir = outdir
+    for part in ident:
+        eventdir = eventdir + part + "/"
+        if not os.path.exists(eventdir):
+            os.system("mkdir " + eventdir)
+    os.system("python ~/ADNEXT/tweetprocessing/tweets_2_corpus.py " + infile + " " + eventdir + "tweets" + " " + textcol)
+    os.chdir(eventdir)
+    os.system("colibri-classencode tweets")
+    os.system("colibri-patternmodeller -f tweets.colibri.dat -t 1 -l 1 -o tweets.colibri.indexedpatternmodel")
+    os.system("colibri-patternmodeller -i tweets.colibri.indexedpatternmodel -c tweets.colibri.cls -P > tweets_wordcounts_ranked.txt")
+
