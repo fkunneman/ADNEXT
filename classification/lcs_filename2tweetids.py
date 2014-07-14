f1313ba4861a8e5108157d0cb9afc3b051968a0e
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-14 16:06:22 +0200

    update

--- classification/lcs_filename2tweetids.py
+++ classification/lcs_filename2tweetids.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description = "Program to read lcs output and e
     performance")
 
 parser.add_argument('-t', action = 'store', nargs = '+', required = True, help = "the label dirs") #train tweets
-parser.add_argument('-f', action='store', nargs = '+', required=True, help = "the test tweet files") #test tweets
+parser.add_argument('-f', action='store', required=True, help = "good id file") #test tweets
 # parser.add_argument('-c', action='store', nargs = '+', required=True, help = "the classification files") #test tweets
 # parser.add_argument('-m', action='store', nargs = '+', required=True, help = "the meta files") #for emotiona category tweet ids
 # parser.add_argument('-l', action='store', nargs = '+', required=True, help = "the list of emotion labels")
@@ -33,40 +33,59 @@ ts = re.compile(r"\d{2}:\d{2}:\d{2}")
 for line in background_meta.readlines():
     tokens = line.strip().split("\t")
     if ts.search(tokens[5]):
-        time = tokens[4] + " " + tokens[5]
+        time = tokens[5]
     else:
-        time = tokens[5] + " " + tokens[4]
+        time = tokens[4]
     #print tokens[1],",",time,",",tokens[-1]
-    user_time_text_tid[tokens[1]][time][tokens[-1]] = tokens[0]
+    user_time_text_tid[tokens[6]][time][tokens[-1]] = tokens[0]
     if not time in backgroundfile_uid_time[tokens[1]].keys():
-        backgroundfile_uid_time[tokens[1]][time] = tokens[0]
+        backgroundfile_uid_time[tokens[6]][time] = tokens[0]
     else:
-        backgroundfile_uid_time[tokens[1]][time] = "double"
+        backgroundfile_uid_time[tokens[6]][time] = "double"
 background_meta.close()
 
-print "skimming through tweet files"
-for f in args.f:
-    tweetfile = open(f)
-    for line in tweetfile.readlines():
-        tokens = line.strip().split("\t")
-        time = tokens[2] + " " + tokens[3]
-        try:
-            if not backgroundfile_uid_time[tokens[1]][time] == "double":
-                filename = backgroundfile_uid_time[tokens[1]][time]
-            else:
-                words = tokens[-1].split(" ")
-                new_words = []
-                for w in words:
-                    if re.search("http",w):
-                        new_words.append("URL")
-                    else:
-                        new_words.append(w)
-                #print tokens[1],time," ".join(new_words)
-                filename = user_time_text_tid[tokens[1]][time][" ".join(new_words)]
-                #print filename
-            backgroundfile_tid[filename] = tokens[0]
-        except:
-            continue
+# print "skimming through tweet files"
+# for f in args.f:
+#     tweetfile = open(f)
+#     for line in tweetfile.readlines():
+#         tokens = line.strip().split("\t")
+#         time = tokens[2] + " " + tokens[3]
+#         try:
+#             if not backgroundfile_uid_time[tokens[1]][time] == "double":
+#                 filename = backgroundfile_uid_time[tokens[1]][time]
+#             else:
+#                 words = tokens[-1].split(" ")
+#                 new_words = []
+#                 for w in words:
+#                     if re.search("http",w):
+#                         new_words.append("URL")
+#                     else:
+#                         new_words.append(w)
+#                 #print tokens[1],time," ".join(new_words)
+#                 filename = user_time_text_tid[tokens[1]][time][" ".join(new_words)]
+#                 #print filename
+#             backgroundfile_tid[filename] = tokens[0]
+#         except:
+#             continue
+
+tweetfile = open(args.f)
+for line in tweetfile.readlines():
+    tokens = line.strip().split("\t")
+    if ts.search(tokens[5]):
+        time = tokens[5]
+    else:
+        time = tokens[4]
+    try:
+        if not backgroundfile_uid_time[tokens[6]][time] == "double":
+            filename = backgroundfile_uid_time[tokens[6]][time]
+        else:
+            filename = user_time_text_tid[tokens[6]][time][tokens[7]]
+            #print filename
+        backgroundfile_tid[filename] = tokens[0]
+    except:
+        continue
+tweetfile.close()
+
 
 #for every label
 print "running through labels"
