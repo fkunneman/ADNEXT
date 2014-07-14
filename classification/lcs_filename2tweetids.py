22f32a6a45e5eeec8fd0310f782ce8a8b8f05b98
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-14 16:35:21 +0200

    fix

--- classification/lcs_filename2tweetids.py
+++ classification/lcs_filename2tweetids.py
@@ -60,9 +60,12 @@ for f in args.f:
                 for output in fc.process(tokens[-1]):
                     if re.search("http",output[0]):
                         word = "URL"
+                    elif re.search(r"^@",output[0]):
+                        word = "USER"
                     else:
                         word = output[0]
                     words.append(word)  
+                print " ".join(words)
                 filename = user_time_text_tid[tokens[6]][time][" ".join(words)]
                 #print filename
             backgroundfile_tid[filename] = tokens[0]
