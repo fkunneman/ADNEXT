cee8c8f293ae6efff2691881591f10514b33cb74
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-14 14:52:59 +0200

    update

--- classification/lcs_filename2tweetids.py
+++ classification/lcs_filename2tweetids.py
@@ -5,19 +5,19 @@ import argparse
 parser = argparse.ArgumentParser(description = "Program to read lcs output and evaluate the \
     performance")
 
-parser.add_argument('-t', action = 'store', nargs = '+', required = True, help = "the train files") #train tweets
-parser.add_argument('-c', action='store', nargs = '+', required=True, help = "the classification files") #test tweets
-parser.add_argument('-m', action='store', nargs = '+', required=True, help = "the meta files") #for emotiona category tweet ids
-parser.add_argument('-l', action='store', nargs = '+', required=True, help = "the list of emotion labels")
+parser.add_argument('-t', action = 'store', nargs = '+', required = True, help = "the label dirs") #train tweets
+# parser.add_argument('-c', action='store', nargs = '+', required=True, help = "the classification files") #test tweets
+# parser.add_argument('-m', action='store', nargs = '+', required=True, help = "the meta files") #for emotiona category tweet ids
+# parser.add_argument('-l', action='store', nargs = '+', required=True, help = "the list of emotion labels")
 parser.add_argument('-b', action='store', required=True, help = "the file with background meta") #for background tweet ids
 parser.add_argument('-w', action='store', required=True, help = "dir to write results to")
 args = parser.parse_args()
 
-if not (len(args.t) == len(args.c) and len(args.c) == len(args.m)):
-    print "no equal sizes of label lists, exiting program"
-    quit()
-else:
-    print "label dicts of equal size, proceeding with program"
+# if not (len(args.t) == len(args.c) and len(args.c) == len(args.m)):
+#     print "no equal sizes of label lists, exiting program"
+#     quit()
+# else:
+#     print "label dicts of equal size, proceeding with program"
 num_labels = len(args.t)
 
 #load background dict
@@ -32,19 +32,18 @@ background_meta.close()
 #for every label
 print "running through labels"
 for i in range(num_labels):
-    label = args.m[i]
-    train_file = open(args.t[i])
-    test_file = open(args.c[i])
-    meta_file = open(args.m[i])
+    labeldir = args.t[i]
+    label = labeldir.split("/")[-2]
+    train_file = open(labeldir + "train")
+    test_file = open(labeldir + "test")
+    meta_file = open(labeldir + "meta.txt")
     print i,"of",num_labels,",",label
-
     trainout = open(args.w + label + "_train.txt","w")
     testout = open(args.w + label + "_test.txt","w")
     #get trainids label from metafile
     for line in metafile.readlines():
         trainout.write(line.split("\t")[1] + " " + label + "\n")
     metafile.close()
-
     #obtain background train tweet ids
     print "obtaining background train tweet ids"
     for line in train_file.readlines():
