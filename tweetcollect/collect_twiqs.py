825a1b44f49e274670ed95c22f44b0f6be5ccc6f
Florian Kunneman <f.kunneman@let.ru.nl>
2014-08-28 10:24:46 +0200

    fix

--- tweetcollect/collect_twiqs.py
+++ tweetcollect/collect_twiqs.py
@@ -36,13 +36,13 @@ if args.k == "echtalles":
             hour = "0" + hour
         timeobj = year+month+day+hour
         outfile = args.o + timeobj + ".txt"
-        print "curl -o " + outfile + " \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\' --cookie \'bZWB9XcuZYWTQ8pG\'"
-        os.system("curl -o " + outfile + " \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\' --cookie \'bZWB9XcuZYWTQ8pG\'")
+        print "curl -o " + outfile + " --cookie \'cookie=bZWB9XcuZYWTQ8pG\' \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\'"
+        os.system("curl -o " + outfile + " --cookie \'cookie=bZWB9XcuZYWTQ8pG\' \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\'")
         outlines = []
         while len(outlines) <= 2:
             print "waiting for",outfile
             time.sleep(300)
-            os.system("curl -o " + outfile + " \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\' --cookie \'bZWB9XcuZYWTQ8pG\'")
+            os.system("curl -o " + outfile + " --cookie \'cookie=bZWB9XcuZYWTQ8pG\' \'http://145.100.57.182//cgi-bin/twitter?SEARCH=echtalles&DATE=" + timeobj + "&DOWNLOAD\'")
             outopen = open(outfile)
             outlines = outopen.readlines()
             outopen.close()
