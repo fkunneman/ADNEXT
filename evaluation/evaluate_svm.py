65a34e45e037126ab964e785077cc376439f6d82
Florian Kunneman <f.kunneman@gmail.com>
2014-09-02 13:44:46 +0200

    fix

--- evaluation/evaluate_svm.py
+++ evaluation/evaluate_svm.py
@@ -69,9 +69,11 @@ if args.f:
     #write tweets
     rankfile = codecs.open("/".join(args.w.split("/")[:-1]) + "/ranked_tweets.txt" ,"w","utf-8")
     for instance in evaluation.return_ranked_score(args.f):
+        print instance
         rankfile.write("\t".join(instance) + "\n")
     fpfile = codecs.open("/".join(args.w.split("/")[:-1]) + "/ranked_fps.txt" ,"w","utf-8")
     for instance in evaluation.return_rankedfp(args.f,"1.0"):
+        print instance
         fpfile.write("\t".join(instance) + "\n")
     rankfile()
     fpfile()
