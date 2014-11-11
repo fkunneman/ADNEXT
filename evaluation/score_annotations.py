de8028a366226d8dce99827ffc78c7fdd110bc39
Florian Kunneman <f.kunneman@let.ru.nl>
2014-11-11 09:21:46 +0100

    fix

--- evaluation/score_annotations.py
+++ evaluation/score_annotations.py
@@ -16,7 +16,7 @@ parser.add_argument('--ka', action = 'store_true', help = "specify to calculate 
 parser.add_argument('--fs', action = 'store_true', help = "specify to calculate mutual F-score")
 #parser.add_argument('-c', action = 'store', nargs = '+', default = [0], help = "[OPTIONAL] if specific columns in a file need to be processed, specify them (one integer denotes every column from this point onwards)")
 #parser.add_argument('-d', action='store_true', help = "choose for a prudent scoring (annotations with doubt are not taken as positive)")
-#parser.add_argument('-p', action='store', required = False, nargs = '+', help = "[OPTIONAL] for a precision-at-curve, specify the index in the given order of excel sheets (just 0 for txt) and the name of the output file")
+parser.add_argument('-p', action='store', required = False, nargs = '+', help = "[OPTIONAL] for a precision-at-curve, specify the index in the given order of excel sheets (just 0 for txt) and the name of the output file")
 parser.add_argument('-s', action = 'store', nargs = '+', required = False, help = "[EXCEL] specify indexes of the excel sheets to be processed")
 
 args = parser.parse_args()
