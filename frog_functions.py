ee727bd78080734c3608c56ffb0868d97f1f964e
Florian Kunneman <f.kunneman@let.ru.nl>
2014-12-10 13:36:14 +0100

    fix

--- frog_functions.py
+++ frog_functions.py
@@ -3,7 +3,7 @@ import frog
 
 class Frogger:
 
-    def __init__(self):
+    def __init__(self,t):
         self.frogger = frog.Frog(frog.FrogOptions(threads=t),"/vol/customopt/uvt-ru/etc/frog/frog-twitter.cfg")
 
     def return_entities(self,sentences):
