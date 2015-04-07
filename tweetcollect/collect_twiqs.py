615827d395398d4239dcac6e718a8b966c86bab8
Florian Kunneman <f.kunneman@let.ru.nl>
2015-04-07 17:43:46 +0200

    update

--- tweetcollect/collect_twiqs.py
+++ tweetcollect/collect_twiqs.py
@@ -11,7 +11,7 @@ Script to collect tweets within a timeframe from twiqs
 """
 parser = argparse.ArgumentParser(description = 
     "Script to collect tweets within a timeframe from twiqs")
-parser.add_argument('-k', action = 'store', required = True, help = "the keyword")
+parser.add_argument('-k', action = 'store', nargs = '+', required = True, help = "the keywords")
 parser.add_argument('-u', action = 'store', required = True, help = "twiqs username")
 parser.add_argument('-p', action = 'store', required = True, help = "twiqs password") 
 parser.add_argument('-s', action = 'store', required = True, help = 
@@ -40,13 +40,13 @@ def request_tweets(t):
             output1st = False
     return output1st
 
-def process_request(t1,t2):
-    payload = {'SEARCH': args.k, 'DATE': t1 + "-" + t2, 'DOWNLOAD':True, 'SHOWTWEETS':True}
+def process_request(t1,t2,k):
+    payload = {'SEARCH': k, 'DATE': t1 + "-" + t2, 'DOWNLOAD':True, 'SHOWTWEETS':True}
     print("fetching",payload["SEARCH"],"in",payload['DATE'],"from twiqs")
     output = False
     while not output:
         output = request_tweets(payload)
-    dumpoutput = '#user_id\t#tweet_id\t#date\t#time\t#reply_to_tweet_id\t#retweet_to_tweet_id\t#user_name\t#tweet\t#DATE='+payload['DATE']+'\t#SEARCHTOKEN=' + args.k + '\n'
+    dumpoutput = '#user_id\t#tweet_id\t#date\t#time\t#reply_to_tweet_id\t#retweet_to_tweet_id\t#user_name\t#tweet\t#DATE='+payload['DATE']+'\t#SEARCHTOKEN=' + k + '\n'
     if output.text[:1000] == dumpoutput: #If there isn't any tweet try the request again for 10 times.
         for i in range(0,requestloop):
             output = False
@@ -63,31 +63,32 @@ def process_request(t1,t2):
     else:
         return output.text
 
-if args.k == "echtalles":
-    current = datetime.datetime(int(args.s[:4]),int(args.s[4:6]),int(args.s[6:8]),int(args.s[8:]),0,0)
-    end = datetime.datetime(int(args.f[:4]),int(args.f[4:6]),int(args.f[6:8]),int(args.f[8:]),0,0)
-    while current <= end:
-        year = str(current.year)
-        month = str(current.month)
-        day = str(current.day)
-        hour = str(current.hour)
-        if len(month) == 1:
-            month = "0" + month
-        if len(day) == 1:
-            day = "0" + day
-        if len(hour) == 1:
-            hour = "0" + hour
-        timeobj = year+month+day+hour
-        tweets = process_request(timeobj,timeobj)
+for k in args.k:
+    if k == "echtalles":
+        current = datetime.datetime(int(args.s[:4]),int(args.s[4:6]),int(args.s[6:8]),int(args.s[8:]),0,0)
+        end = datetime.datetime(int(args.f[:4]),int(args.f[4:6]),int(args.f[6:8]),int(args.f[8:]),0,0)
+        while current <= end:
+            year = str(current.year)
+            month = str(current.month)
+            day = str(current.day)
+            hour = str(current.hour)
+            if len(month) == 1:
+                month = "0" + month
+            if len(day) == 1:
+                day = "0" + day
+            if len(hour) == 1:
+                hour = "0" + hour
+            timeobj = year+month+day+hour
+            tweets = process_request(timeobj,timeobj,k)
+            if tweets != "":
+                outfile = codecs.open(args.o + timeobj + ".txt","w","utf-8")
+                outfile.write(tweets)
+                outfile.close()
+                current = current + datetime.timedelta(hours = 1)
+
+    else:
+        tweets = process_request(args.s,args.f,k)
         if tweets != "":
-            outfile = codecs.open(args.o + timeobj + ".txt","w","utf-8")
+            outfile = codecs.open(args.o + k + "-" + args.s[:6] + "-" + args.f[:6] + ".txt","w","utf-8")
             outfile.write(tweets)
             outfile.close()
-            current = current + datetime.timedelta(hours = 1)
-
-else:
-    tweets = process_request(args.s,args.f)
-    if tweets != "":
-        outfile = codecs.open(args.o + args.k + ".txt","w","utf-8")
-        outfile.write(tweets)
-        outfile.close()
