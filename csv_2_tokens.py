e3cef72f2032526bbacd33c4f26cd084bc4a1c0a
Florian Kunneman <f.kunneman@let.ru.nl>
2015-05-11 17:35:13 +0200

    update

--- csv_2_tokens.py
+++ csv_2_tokens.py
@@ -4,32 +4,60 @@ import csv
 import sys
 import frog
 
+fo = frog.FrogOptions(threads=20)
+frogger = frog.Frog(fo,"/vol/customopt/uvt-ru/etc/frog/frog-twitter.cfg")
 csv.field_size_limit(sys.maxsize)
+
+filename = sys.argv[1][:-4]
+
+metalist = []
+tokenslist = []
+stemslist = []
+poslist = []
+
 with open(sys.argv[1], 'r') as csvfile:
     reader = csv.reader(csvfile)
-    for row in reader:
-        print(row)
+    for row in reader[:20]:
+        tokens = []
+        stems = []
+        pos = []
+        metalist.append(row[:-1])
+        data = frogger.process(row[-1])
+        for token in data:
+                tokens.append(token["text"])
+                pos.append(token["pos"])
+                stems.append(token["lemma"])
+        tokenslist.append(tokens)
+        poslist.append(tokens)
+        stemslist.append(tokens)
 
-# infile = open(sys.argv[1],encoding="utf-8")
+with open(filename + "tokenized.csv", 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for i,tokens in enumerate(tokenslist):
+        writer.writerow(metalist[i] + [" ".join(tokens)])
 
-# fo = frog.FrogOptions(threads=20)
-# frogger = frog.Frog(fo,"/vol/customopt/uvt-ru/etc/frog/frog-twitter.cfg")
+with open(filename + "postags.csv", 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for i,tokens in enumerate(poslist):
+        writer.writerow(metalist[i] + [" ".join(tokens)])
 
+with open(filename + "stems.csv", 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for i,tokens in enumerate(stemslist):
+        writer.writerow(metalist[i] + [" ".join(tokens)])
 
-
-# for i,t in enumerate(self.instances):
-#     print("tokenizing",i,"of",num_instances,"instances")
-#     tokens = []
-#     stems = []
-#     poss = []
-#     data = frogger.process(t.text)
-#     for token in data:
-#         if (punct and not token["pos"] == "LET()") or \
-#             not punct:
-#             tokens.append(token["text"])
-#             poss.append(token["pos"])
-#             stems.append(token["lemma"])
 #     t.add_stem(stems)
 #     t.add_pos(poss)
 #     t.text = " ".join(tokens)
+
+
+
+
+# infile = open(sys.argv[1],encoding="utf-8")
+
+
+
+
+
+
 
