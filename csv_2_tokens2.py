d535dfd71f21b915473d261f44eaf868d67f0e5e
Florian Kunneman <f.kunneman@let.ru.nl>
2015-05-11 20:24:45 +0200

    test

--- csv_2_tokens2.py
+++ csv_2_tokens2.py
@@ -12,25 +12,27 @@ import utils
 #Function to tokenize the inputfile
 def frogger(ls,o,i):
     fc = pynlpl.clients.frogclient.FrogClient('localhost',sys.argv[2])    
-    
+
+    out = []
     for l in ls:
-        ts = fc.process(l)
-        lemma
-        print(output)
-        #words.append(word)    
-        #outstring = outstring + "\n"
-        #o.put(outstring)
-    
-    print("Chunk ",i," done.")
+        lemmas = []
+        tokens = []
+        poss = []
+        frs = fc.process(l[1])
+        for t in frs:
+            if t[0]:
+                tokens.append(t[0])
+                lemmas.append(t[1])
+                poss.append(t[3])
+        o.put(l[0]," ".join(tokens)," ".join(lemmas)," ".join(poss))
+
+    print("chunk",i,"done")
 
 csv.field_size_limit(sys.maxsize)
 filename = sys.argv[1][:-4]
 
 lines = []
-metalist = []
-tokenslist = []
-stemslist = []
-poslist = []
+
 
 print("reading in file")
 #dataset = utils.load_data(filename=sys[1], random_state=None, max_n=None)
@@ -41,29 +43,8 @@ with open(sys.argv[1], 'r') as csvfile:
         lines.append(row)
 
 #texts = dataset["texts"]
-texts = [i[-1] for i in lines]
+texts = [i[-1] for i in lines][:20]
 indices = range(len(texts))
-
-print("Frogging")
-fc = pynlpl.clients.frogclient.FrogClient('localhost',sys.argv[2])    
-
-ts = texts[:2]
-lemmas = []
-tokens = []
-for t in ts:
-    frs = fc.process(t)
-    for t in frs:
-        if t[0]:
-            lemmas.append(t[1])
-            tokens.append(t[0])
-#print(t[0] for t in tokens)
-    #print(t[1] for t in tokens)
-    #print(t[3] for t in tokens)
-# for t in texts[:20]:    
-#     for output in fc.process(t):
-#         print(output)
-print(zip(tokens,lemmas))
-quit()
 
 print("Processing lines.")
 q = multiprocessing.Queue()
@@ -74,13 +55,15 @@ for i in range(len(tweets_chunks)):
     p = multiprocessing.Process(target=frogger,args=[tweets_chunks[i],q,i])
     p.start()
 
+total = len(lines)
 while True:
     l = q.get()
     frogged.append(l)
-    print(len(frogged))
+    print(len(frogged),"of",total)
     if len(frogged) == len(indices):
         break
 
+print("writing to files")
 frogged_sorted = sorted(frogged,key = lambda k : k[0])
 meta = []
 for index in [i[0] for i in frogged_sorted]:
@@ -91,14 +74,14 @@ for index in [i[0] for i in frogged_sorted]:
 with open(filename + "_tokenized.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(frogged_sorted):
-        writer.writerow(meta[i] + [" ".join(tokens[1])])
+        writer.writerow(meta[i] + [tokens[1]])
 
 with open(filename + "_lemmatized.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(frogged_sorted):
-        writer.writerow(meta[i] + [" ".join(tokens[2])])
+        writer.writerow(meta[i] + [tokens[2]])
 
 with open(filename + "_postags.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(frogged_sorted):
-        writer.writerow(meta[i] + [" ".join(tokens[3])])
+        writer.writerow(meta[i] + [tokens[3]])
