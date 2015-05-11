0fa3534df694fc0eed6c69552aad4793fb4401ef
Florian Kunneman <f.kunneman@gmail.com>
2015-05-11 19:17:08 +0200

    fix

--- csv_2_tokens.py
+++ csv_2_tokens.py
@@ -10,38 +10,46 @@ csv.field_size_limit(sys.maxsize)
 
 filename = sys.argv[1][:-4]
 
+lines = []
 metalist = []
 tokenslist = []
 stemslist = []
 poslist = []
 
+print("reading in file")
 with open(sys.argv[1], 'r') as csvfile:
     reader = csv.reader(csvfile)
-    for row in reader[:20]:
-        tokens = []
-        stems = []
-        pos = []
-        metalist.append(row[:-1])
-        data = frogger.process(row[-1])
-        for token in data:
-                tokens.append(token["text"])
-                pos.append(token["pos"])
-                stems.append(token["lemma"])
-        tokenslist.append(tokens)
-        poslist.append(tokens)
-        stemslist.append(tokens)
+    for row in reader:
+        lines.append(row)
 
-with open(filename + "tokenized.csv", 'w') as csvfile:
+print("frogging")
+l = len(lines)
+for i,line in enumerate(lines):
+    print("line",i,"of",l)   
+    tokens = []
+    stems = []
+    pos = []
+    metalist.append(line[:-1])
+    data = frogger.process(line[-1])
+    for token in data:
+        tokens.append(token["text"])
+        pos.append(token["pos"])
+        stems.append(token["lemma"])
+    tokenslist.append(tokens)
+    poslist.append(tokens)
+    stemslist.append(tokens)
+
+with open(filename + "_tokenized.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(tokenslist):
         writer.writerow(metalist[i] + [" ".join(tokens)])
 
-with open(filename + "postags.csv", 'w') as csvfile:
+with open(filename + "_postags.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(poslist):
         writer.writerow(metalist[i] + [" ".join(tokens)])
 
-with open(filename + "stems.csv", 'w') as csvfile:
+with open(filename + "_stems.csv", 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for i,tokens in enumerate(stemslist):
         writer.writerow(metalist[i] + [" ".join(tokens)])
